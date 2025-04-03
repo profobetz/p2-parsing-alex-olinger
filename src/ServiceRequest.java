069023b6d7b7626e6b2ac815d5e25f682f86da58
Alex Olinger <alexolingervideo@gmail.com>
2025-04-03 15:44:08 -0400

    Added ServiceRequest logic/attr

---
+++ src/ServiceRequest.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class ServiceRequest {
+    LocalDate date_opened;
+    LocalDate date_closed;
+    boolean was_closed_on_time;
+    String reason_for_request;
+    String neighborhood;
+
+    ServiceRequest(LocalDate date_opened, LocalDate date_closed, String reason_for_request, String neighborhood) {
+        this.date_opened = date_opened;
+        this.date_closed = date_closed;
+        this.reason_for_request = reason_for_request;
+        this.neighborhood = neighborhood;
+    }
+
+    public int getDaysOpen() {
+        Period period = Period.between(date_opened, date_closed);
+        int days = period.getDays();
+        return days;
+    }
+
+
+
+}
